357ebadc8fca1f5eeacc192ef21cf0e774352bd4
Augusto Occhiuzzi <96669705+augustoocch@users.noreply.github.com>
2022-02-09 10:45:19 -0300

    Update Conexion.java

--- java/datos/Conexion.java
+++ java/datos/Conexion.java
@@ -9,7 +9,7 @@ public class Conexion {
     
 private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 private static final String JDBC_USER = "root";
-private static final String JDBC_PASSWORD = "Bigua205";
+private static final String JDBC_PASSWORD = "xxxxx";
 
 
 //SE CONECTA A LA BASE DE DATOS
